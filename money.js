eb6f2065754c112c5c948f04010157f01b04224a
bird-thing <jklein13124@gmail.com>
2025-07-22 16:03:05 -0400

    kfdsjkfjdsjkfsdkljfkjsdlfj

--- money.js
+++ money.js
@@ -1,20 +1,20 @@
 
 if (localStorage.getItem("money_count") == null) {
-    var money = 0;
-    localStorage.setItem("money_count", money);
-  } else {
-    localStorage.getItem("money_count");
-    money = localStorage.getItem("money_count");
+  var money = 0;
+  localStorage.setItem("money_count", money);
+} else {
+  localStorage.getItem("money_count");
+  money = localStorage.getItem("money_count");
+}
+var moneys_text;
+function giveMoney() {
+  money++;
+  localStorage.setItem("money_count", money);
+  document.getElementById("money_display_text").innerHTML =
+    "Amount Gained = " + localStorage.getItem("money_count");
+  if (money == null) {
+    money = 0;
   }
-  var moneys_text;
-  function giveMoney() {
-    money++;
-    localStorage.setItem("money_count", money);
-    document.getElementById("money_display_text").innerHTML =
-      "Amount Gained = " + localStorage.getItem("money_count");
-    if (money == null) {
-      money = 0;
-    }
-  
-    console.log(money);
-  }
+
+  console.log(money);
+}
---
+++ random.js
@@ -0,0 +1,30 @@
+// let loops = 1;
+// let digits = 10;
+// let tens = 1;
+// function onframe(){
+  
+//     if(loops==10*tens){
+//         digits = digits * 10;
+//         if(tens<10){
+//         tens++;}
+//         if(tens>10){
+//             tens=-1;
+//         }
+
+//     }
+    
+//    // document.getElementById("rando").innerHTML = randomNum;
+//    let randomNum = Math.round(Math.random()*digits)
+    
+//     requestAnimationFrame(onframe);
+//    if(randomNum == null){
+//     randomNum = Math.round(Math.random()*digits)
+//    }
+   
+//    document.getElementById("rando").innerHTML = randomNum;
+  
+//    loops++;
+   
+   
+// }
+// onframe();
