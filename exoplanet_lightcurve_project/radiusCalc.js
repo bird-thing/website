d790a857543ab5a6d9a622047fb6545363ac46f4
bird-thing <jklein13124@gmail.com>
2025-07-29 14:25:44 -0400

    d

---
+++ exoplanet_lightcurve_project/radiusCalc.js
@@ -0,0 +1,21 @@
+let solarRadius; // times the size of the SUN
+let transitDepth; // percent dip of light
+function doMath(){
+   if(solarRadius == null){
+      solarRadius = 2; //2 times the size of the sun
+
+   }
+   if(transitDepth == null){
+      transitDepth = 0.006; // 0.6% dips
+   }
+   solarRadius = document.getElementById("solarRadii").value
+   transitDepth = document.getElementById("depth").value;
+   let x = Math.sqrt(transitDepth)*solarRadius*100000;
+   let y = Math.round(x)/10000;
+   document.getElementById("planetRadius").innerHTML = y+" jupiter radii (how many jupiters it is)";
+}
+// oddly, despite having fresh data in it and correct equations... the results from here differ from
+// data i found online about the sample planets.
+// its possible either source of data is incorrect or outdated.
+// maybe do a double check on the equation? or what data im putting in
+// but  it should be correct. not sure why its a bit different but in the ballpark.
